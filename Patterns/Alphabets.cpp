48c0c8a9d1c86fabccd1247fc5a487471fc967ab
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-07-24 13:45:00 +0530

    Create Alphabets.cpp
    n = 3
    A A A
    B B B
    C C C

---
+++ Patterns/Alphabets.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=n;j++){
+			cout << (char)(65 + i - 1) << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
