38e9e32dbc5c03c9b5d8a25d9d1cfe730a2487a9
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-07-24 13:56:43 +0530

    Update ProgressingRightHalfTriangleAlphabets.cpp

--- Patterns/ProgressingRightHalfTriangleAlphabets.cpp
+++ Patterns/ProgressingRightHalfTriangleAlphabets.cpp
@@ -18,6 +18,22 @@ int main(){
 		cout << endl;
 	}
 	*/
+	
+	/*
+	n = 3
+	C
+	B C
+	A B C
+	for(int i=1;i<=n;i++){
+		char ch = 'A' + n - i;
+		for(int j=1;j<=i;j++){
+			cout << ch << " ";
+			ch = ch + 1;
+		}
+		cout << endl;
+	}
+	*/
+	
 	/*
 	n = 3
 	A
