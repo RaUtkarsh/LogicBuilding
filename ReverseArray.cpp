e68038efed615fc9f90b94ba2359a00080e90b95
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-07-25 13:21:52 +0530

    Update ReverseArray.cpp

--- ReverseArray.cpp
+++ ReverseArray.cpp
@@ -23,3 +23,41 @@ int main(){
 	reverseArray(arr, 0, n-1);
 	printArray(arr, n);
 }
+
+
+//Another easier way
+
+
+#include<iostream>
+using namespace std;
+
+void reverse(int arr[], int n){
+	
+	int start = 0, end = n-1;
+	
+	while(start <= end){
+		swap(arr[start], arr[end]);
+		start++;
+		end--;
+	}
+}
+
+void printArray(int arr[], int n){
+	
+	for(int i=0;i<n;i++){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+
+int main(){
+	
+	int arr[10] = {15, 12, 10, 16, 0, 23, 14, 5, 18, 39};
+	
+	reverse(arr, 10);
+	
+	printArray(arr, 10);
+	
+	
+	return 0;
+}
