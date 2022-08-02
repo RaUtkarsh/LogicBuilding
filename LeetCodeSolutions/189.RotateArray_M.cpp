4d1ddf089861ed4f292add42814e0e0e45f54587
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-08-02 19:04:57 +0530

    Update 189.RotateArray_M.cpp

--- LeetCodeSolutions/189.RotateArray_M.cpp
+++ LeetCodeSolutions/189.RotateArray_M.cpp
@@ -1,7 +1,13 @@
+//Logic
+//When you mod any number with n the value returned is always in range 0 to (n-1)
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+        
+        //create another array so elements do not get overwritten
         vector<int> temp(nums.size());
+        
         for(int i=0;i<nums.size();i++){
             temp[(i+k)%nums.size()] = nums[i];
         }
