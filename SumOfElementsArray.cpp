25726544dfdc10d8261af1e9d06276d4fe910008
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-08-13 13:53:46 +0530

    Update SumOfElementsArray.cpp

--- SumOfElementsArray.cpp
+++ SumOfElementsArray.cpp
@@ -20,3 +20,39 @@ int main(){
 	return 0;
 	
 }
+
+
+
+//2nd Approach
+//Using recursion
+
+#include <iostream>
+
+using namespace std;
+
+int getSum(int *arr, int n){
+    
+    if(n == 0){
+        return 0;
+    }
+    if(n == 1){
+        return arr[0];
+    }
+    
+    int remainingSum = getSum(arr+1, n-1);
+    int sum = arr[0] + remainingSum;
+    
+    return sum;
+}
+
+int main()
+{
+   
+    int arr[5] = {3, 5, 5, 1, 6};
+    
+    int sum = getSum(arr, 5);
+    
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
