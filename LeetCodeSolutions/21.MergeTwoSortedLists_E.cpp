ac9fbfb395cdeda076fc3ba59c755cffbcfa58b8
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-08-21 17:59:05 +0530

    Update 21.MergeTwoSortedLists_E.cpp

--- LeetCodeSolutions/21.MergeTwoSortedLists_E.cpp
+++ LeetCodeSolutions/21.MergeTwoSortedLists_E.cpp
@@ -65,16 +65,6 @@ public:
 //RECURSIVE APPROACH
 
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
