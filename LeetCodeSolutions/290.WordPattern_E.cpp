f04c00b4f0dae94b928ea4b69274eeab3df357de
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-08-25 12:11:21 +0530

    Create 290.WordPattern_E.cpp

---
+++ LeetCodeSolutions/290.WordPattern_E.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        
+        vector<string> v;
+        string word;
+        
+        stringstream sts(s);
+    
+        while(sts>>word){
+            v.push_back(word);
+        }
+        
+        if(v.size()!=pattern.size()){
+            return false;
+        }
+        
+        unordered_map<char, string> m;
+        set<string> st;
+        
+        for(int i=0;i<pattern.size();i++){
+            
+            if(m.find(pattern[i])!=m.end()){
+                
+                if(m[pattern[i]]!=v[i]){
+                    return false;
+                }
+            }
+            else{
+                if(st.count(v[i])>0){
+                    return false;
+                }
+                m[pattern[i]]= v[i];
+                st.insert(v[i]);
+            }
+        
+        }
+        return true;
+
+    }
+};
