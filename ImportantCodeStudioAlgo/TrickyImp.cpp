53a7a29e7b3761ba3f12a5464b8b69c6e7112672
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-09-05 17:53:33 +0530

    Create TrickyImp.cpp

---
+++ ImportantCodeStudioAlgo/TrickyImp.cpp
@@ -0,0 +1,24 @@
+
+from sys import stdin
+
+def removeConsecutiveDuplicates(string) :
+	# Your code goes here
+
+    seen = string[0]
+    ans = string[0]
+    for i in string[1:]:
+        if i != seen:
+            ans += i
+            seen = i
+    return ans
+
+
+	
+
+
+#main
+string = stdin.readline().strip()
+
+ans = removeConsecutiveDuplicates(string)
+
+print(ans)
