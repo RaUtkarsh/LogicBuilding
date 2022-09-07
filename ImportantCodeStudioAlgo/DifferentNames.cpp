1816666a913e216dafeb645ced8edee658c112b8
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-09-07 12:14:34 +0530

    Create DifferentNames.cpp

---
+++ ImportantCodeStudioAlgo/DifferentNames.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write your code here
+    string s;
+    int count=0;    
+    
+    getline(cin,s);
+    
+    string temp="";
+    
+    unordered_map<string,int> m;
+    for(int i=0;i<s.length();i++){
+        if(s[i]==' '){
+            
+            m[temp]++;
+            temp="";
+        }else{
+            temp=temp+s[i];
+        }
+        if(i==s.length()-1){
+            m[temp]++;
+        }
+    }
+    
+    unordered_map<string,int>::iterator it;
+    for(it=m.begin();it!=m.end();it++){
+        if(it->second>=2){
+            
+            cout<<it->first<<" "<<it->second<<endl;
+            count++;
+        }
+    }
+    if(count==0)
+        cout<<-1;  
+    return 0;
+    }
