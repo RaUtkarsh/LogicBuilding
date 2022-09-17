e68ebdf0d7f04ab5b3e1178706c8fd1c9a1a4d69
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-09-17 10:52:23 +0530

    Update Sort012InLinkedList.cpp

--- ImportantCodeStudioAlgo/Sort012InLinkedList.cpp
+++ ImportantCodeStudioAlgo/Sort012InLinkedList.cpp
@@ -48,3 +48,69 @@ Node* sortList(Node *head)
     }
     return head;
 }
+
+
+//Another approach
+//sort without swapping the data
+
+/********************************
+    class Node
+    {
+    public:
+        int data;
+        Node *next;
+        Node(int data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+    };
+
+********************************/
+void insertAtTail(Node* &tail, Node* curr){
+    
+    tail -> next = curr;
+    tail = curr;
+}
+Node* sortList(Node *head)
+{
+    // Write your code here.
+    Node* zeroHead = new Node(-1);
+    Node* zeroTail = zeroHead;
+    Node* oneHead = new Node(-1);
+    Node* oneTail = oneHead;
+    Node* twoHead = new Node(-1);
+    Node* twoTail = twoHead;
+    
+    Node* curr = head;
+    while(curr != NULL){
+        int value = curr -> data;
+        if(value == 0){
+            insertAtTail(zeroTail, curr);
+        }
+        else if(value == 1){
+            insertAtTail(oneTail, curr);
+        }
+        else if(value == 2){
+            insertAtTail(twoTail, curr);
+        }
+        curr = curr -> next;
+    }
+    
+    if(oneHead -> next != NULL){
+        zeroTail -> next = oneHead -> next;
+    }
+    else{
+        zeroTail -> next = twoHead -> next;
+    }
+    
+    oneTail -> next = twoHead -> next;
+    twoTail -> next = NULL;
+    
+    head = zeroHead -> next;
+    delete zeroHead;
+    delete oneHead;
+    delete twoHead;
+    
+    return head;
+}
