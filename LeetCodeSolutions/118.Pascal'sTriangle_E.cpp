b85ac842bfeb6525604a186fa3bb8d2dda25b3b1
Utkarsh Raj <86511235+RaUtkarsh@users.noreply.github.com>
2022-10-21 09:20:57 +0530

    Update 118.Pascal'sTriangle_E.cpp

--- LeetCodeSolutions/118.Pascal'sTriangle_E.cpp
+++ LeetCodeSolutions/118.Pascal'sTriangle_E.cpp
@@ -11,3 +11,21 @@ public:
         return res;
     }
 };
+
+
+//2nd Approach
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res;
+        for(int i = 0; i < numRows; i++){
+            vector<int> ans(i+1, 1);
+            for(int j = 1; j < i; j++){
+                ans[j] = res[i-1][j] + res[i-1][j-1];
+            }
+            res.push_back(ans);
+        }
+        return res;
+    }
+};
